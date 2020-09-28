09fc0b66884f0d4b7adb09d88fa7c01d72d6236a
fangkun119 <fangkun119@hotmail.com>
2020-09-28 21:00:43 +0800

    01 spring

--- 01_spring/02_mvn_and_setter_inject/src/main/java/com/javaref/spring/TestGetBean.java
+++ 01_spring/02_mvn_and_setter_inject/src/main/java/com/javaref/spring/TestGetBean.java
@@ -1,15 +1,16 @@
-package com.mashibing.spring;
+package com.javaref.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-// Note: ToStringBuilder是很好用的工具类，减少代码长度
+
+// ToStringBuilder是很好用的工具类，减少代码长度
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class TestGetBean {
 	public static void main(String[] args) {
-		// Note: 指定生成ClassPathXmlApplicationContext的Spring配置文件
+		// 指定生成ClassPathXmlApplicationContext的Spring配置文件
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-		// Note: 根据配置注入Bean
+		// 根据配置注入Bean
 		Person person1 = ctx.getBean("person1_alias", Person.class);
 		System.out.println(ToStringBuilder.reflectionToString(person1));
 
