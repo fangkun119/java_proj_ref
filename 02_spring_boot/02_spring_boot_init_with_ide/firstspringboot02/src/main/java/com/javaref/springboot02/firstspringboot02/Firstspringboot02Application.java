b043e75fc48d664896a7721386e528dce2abf2cd
fangkun119 <fangkun119@hotmail.com>
2020-10-06 12:05:43 +0800

    02 Spring Boot

---
+++ 02_spring_boot/02_spring_boot_init_with_ide/firstspringboot02/src/main/java/com/javaref/springboot02/firstspringboot02/Firstspringboot02Application.java
@@ -0,0 +1,13 @@
+package com.javaref.springboot02.firstspringboot02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Firstspringboot02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Firstspringboot02Application.class, args);
+    }
+
+}
---
+++ 02_spring_boot/02_spring_boot_init_with_ide/firstspringboot02/src/test/java/com/javaref/springboot02/firstspringboot02/Firstspringboot02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaref.springboot02.firstspringboot02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Firstspringboot02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
