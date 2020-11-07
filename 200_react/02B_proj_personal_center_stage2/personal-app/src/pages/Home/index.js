8b70dcbd32e5828b42bb9cb89bb8e0b15c23e073
fangkun119 <fangkun119@hotmail.com>
2020-11-07 16:28:18 +0800

    [200 React] proj02b personal center stg2: home page 03, user info card

--- 200_react/02B_proj_personal_center_stage2/personal-app/src/pages/Home/index.js
+++ 200_react/02B_proj_personal_center_stage2/personal-app/src/pages/Home/index.js
@@ -1,9 +1,13 @@
 import React, {useState} from 'react';
 import { Row, Col, Card } from 'antd';
+import { ContactsOutlined, ClusterOutlined, HomeOutlined } from '@ant-design/icons'
 import Articles from '../../components/Articles';
 import Projects from '../../components/Projects';
 import Applications from '../../components/Applications';
 import styles from './index.module.less';
+
+// 后端没有开发好，因此先引入预先构造好的假数据文件，用于调试
+import {currentUser, fakeList} from './data.js';
 
 const operationTabList = [{
     key: 'articles',
@@ -19,13 +23,35 @@ const operationTabList = [{
 const renderChildrenByTabKey = (tabKey) => {
     switch (tabKey) {
         case 'articles': 
-            return <p>Articles</p>
+            return <Articles/>
         case 'applications':
-            return <p>Applications</p>
+            return <Applications/>
         case 'projects': 
-            return <p>Projects</p>
+            return <Projects/>
+        default:
+            return <p></p>
     }
 };
+
+// () => (<JSX_ELEM/>) 相当于 () => {return (<JSX_ELEM/>)}
+// <p><图标/>文字</p>
+const renderUserInfo = () => (
+    <div className={styles.detail} >
+        <p>
+            <ContactsOutlined className={styles.userInfoIcon} />
+            {currentUser.title} 
+        </p>
+        <p>
+            <ClusterOutlined className={styles.userInfoIcon} />
+            {currentUser.group}
+        </p>
+        <p>
+            <HomeOutlined className={styles.userInfoIcon} /> 
+            {(currentUser.geographic || { province: {label: ''}}).province.label}
+            {(currentUser.geographic || { city: {label: ''}}).city.label}
+        </p>  
+    </div>
+);
 
 const Home = () => {
     const [tabKey, setTabKey] = useState('articles');
@@ -42,7 +68,12 @@ const Home = () => {
             <Row gutter={24}>
                 <Col lg={7} md={24}>
                     <Card bordered={false} style={{marginBottom:24}}>
-                        col1
+                        <div className={styles.avatarHolder}>
+                            <img alt="" src={currentUser.avatar} />
+                            <div className={styles.name}>{currentUser.name}</div>
+                            <div>{currentUser.signature}</div>
+                        </div>
+                        {renderUserInfo()}
                     </Card> 
                 </Col>
                 <Col lg={17} md={24}>
