3c5223199e856aea58732d839cd12e6ab9a3d50d
fangkun119 <fangkun119@hotmail.com>
2020-11-10 17:40:36 +0800

    [200 React] proj02b personal center stg2: apply redux (framework part)

--- 200_react/02B_proj_personal_center_stage2/personal-app/src/index.js
+++ 200_react/02B_proj_personal_center_stage2/personal-app/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
+import { createStore, applyMiddleware } from 'redux'; //用来注入reducers
+import thunk from 'redux-thunk'; //异步操作中间件 
+import { StoreContext, storeContext } from 'redux-react-hook'; //连接react和redux的桥
+
+import App from './App';
+import reducers from './reducers';
 import './index.less'; 
 
+const store = createStore(
+  reducer, //注入src/reducers/index.js中编写的reducers
+  applyMiddleware(thunk) //应用异步中间件
+)
+
+// 用<StoreContext.Provider value={store}>包裹<App>，这样App的各处代码都能取到store的数据
 ReactDOM.render(
-  <App />
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>
   ,
   document.getElementById('root')
 );
---
+++ 200_react/02B_proj_personal_center_stage2/personal-app/src/reducers/index.js
@@ -0,0 +1,11 @@
+// ruducers/indes.js: 用于数据计算和存储
+
+// 用来把多层reducer组合起来
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+
+});
+
+// export出去，由入口文件index.js来import
+export default rootReducer;
