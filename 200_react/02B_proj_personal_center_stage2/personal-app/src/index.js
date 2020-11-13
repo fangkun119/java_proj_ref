881e67ea4972e84e2d7640dcbac589c77c6d2e04
fangkun119 <fangkun119@hotmail.com>
2020-11-13 22:18:26 +0800

    [200 React] proj02b personal center stg2: apply reduce dev plug-in to check data in reducer

--- 200_react/02B_proj_personal_center_stage2/personal-app/src/index.js
+++ 200_react/02B_proj_personal_center_stage2/personal-app/src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom';
 
 import { createStore, applyMiddleware } from 'redux'; //用来注入reducers
 import thunk from 'redux-thunk'; //异步操作中间件 
-import { StoreContext, storeContext } from 'redux-react-hook'; //连接react和redux的桥
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { StoreContext } from 'redux-react-hook'; //连接react和redux的桥
 
 import App from './App';
 import reducer from './reducers'; 
 import './index.less';   
 
 const store = createStore(
-  reducer, //注入src/reducers/index.js中编写的reducers
-  applyMiddleware(thunk) //应用异步中间件
+  // 注入src/reducers/index.js中编写的root reducers
+  reducer, 
+  // 用Dev Tools包裹：可以加一个判断，仅在开发环境采用Dev Tools包裹
+  composeWithDevTools (
+    applyMiddleware(thunk) // 异步中间件
+  )
 )
 
 // 用<StoreContext.Provider value={store}>包裹<App>，这样App的各处代码都能取到store的数据
