9fe9cf820ea42bd9e0b011b93ee8681a927af071
fangkun119 <fangkun119@hotmail.com>
2020-11-15 21:22:45 +0800

    [200 React] proj03 weibo client: use scss to adjust web style

--- 200_react/03_proj_weibo_client/weibo-app/src/index.js
+++ 200_react/03_proj_weibo_client/weibo-app/src/index.js
@@ -6,6 +6,8 @@ import { StoreContext } from 'redux-react-hook';
 import reducers from './reducers'; // 是在"./reducers/index.js”中被“export default”的RootReducer
 import App from './App';
 
+import './index.scss';
+
 const store = createStore(
   reducers,
   applyMiddleware(thunk) // 异步插件
--- 200_react/03_proj_weibo_client/weibo-app/src/pages/Home/index.js
+++ 200_react/03_proj_weibo_client/weibo-app/src/pages/Home/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useMappedState } from 'redux-react-hook';
 import { getHomeTimeline } from '../../actions/timeline';
+import moment from 'moment';
+
+import styles from './index.module.scss';
 
 const Home = () => {
     const { 
@@ -11,13 +14,42 @@ const Home = () => {
     useEffect(() => {
         dispatch(getHomeTimeline())
     }, [dispatch]); //依赖[dispatch]：是一个规范，一旦dispatch发生变化，可以重新触发actions
-    
+
     return(
         <div>
             {
-                home.map(({id, text}) => (
-                    <div key={id} style={{marginBottom : '10px'}}>
-                        {text}
+                home.map(({id, text, user, created_at, source, pic_urls}) => (
+                    <div className={styles.post} key={id} style={{marginBottom : '10px'}}>
+                        <div className={styles.user} style={{display : 'flex'}}> 
+                            <img 
+                                src={user.profile_image_url} 
+                                className={styles.avatar} 
+                                alt={user.screen_name}
+                            />
+                            <div className={styles.userInfo}>
+                                <div>{user.screen_name}</div>
+                                <div className={styles.extra}>
+                                    {moment(created_at).fromNow()} 来自 <span dangerouslySetInnerHTML={{ __html: source }} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.content}>
+                            <div className={styles.text}>
+                                {text}
+                            </div>
+                            <ul className={styles.images}> 
+                                {
+                                    // <li><ul>是无序列表，但是样式在src/index.scss中已经被修改了
+                                    pic_urls.map(({thumbnail_pic}) => (
+                                        <li key={thumbnail_pic} className={styles.imgWrapper}>
+                                            <div className={styles.imgContainer}>
+                                                <img src={thumbnail_pic} alt={thumbnail_pic} />
+                                            </div>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
                     </div>
                 ))
             }
