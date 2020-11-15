c44ff7f5d450305850c1dcc9ca574ecfe39aceb1
fangkun119 <fangkun119@hotmail.com>
2020-11-15 20:10:14 +0800

    [200 React] proj03 weibo client: use redux to manage backend response: update comment

--- 200_react/03_proj_weibo_client/weibo-app/src/index.js
+++ 200_react/03_proj_weibo_client/weibo-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { StoreContext } from 'redux-react-hook';
-import reducers from './reducers';
+import reducers from './reducers'; // 是在"./reducers/index.js”中被“export default”的RootReducer
 import App from './App';
 
 const store = createStore(
   reducers,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk) // 异步插件
 );
 
 ReactDOM.render(
--- 200_react/03_proj_weibo_client/weibo-app/src/pages/Home/index.js
+++ 200_react/03_proj_weibo_client/weibo-app/src/pages/Home/index.js
@@ -3,12 +3,11 @@ import { useDispatch, useMappedState } from 'redux-react-hook';
 import { getHomeTimeline } from '../../actions/timeline';
 
 const Home = () => {
-
-    const dispatch = useDispatch();
     const { 
         home = [] /*初始值为空，更新值从store中获取，其中会用到下面设置的函数来挑选与“信息流”有关的数据*/
     } =  useMappedState((state) => (state.timeline));
 
+    const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getHomeTimeline())
     }, [dispatch]); //依赖[dispatch]：是一个规范，一旦dispatch发生变化，可以重新触发actions
