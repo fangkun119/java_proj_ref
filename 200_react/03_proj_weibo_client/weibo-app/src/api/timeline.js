b006e40e811a2483a6c280f0757bc1204fa18286
fangkun119 <fangkun119@hotmail.com>
2020-11-16 22:19:56 +0800

    [200 React] proj03 weibo client: fix ajax parameter error

--- 200_react/03_proj_weibo_client/weibo-app/src/api/timeline.js
+++ 200_react/03_proj_weibo_client/weibo-app/src/api/timeline.js
@@ -1,5 +1,5 @@
 import ajax from "../utils/ajax";
 
 export function getHomeTimeline(params) {
-    return ajax.get('/proxy/2/statuses/home_timeline.json', {data: params});
+    return ajax.get('/proxy/2/statuses/home_timeline.json', {params});
 }
