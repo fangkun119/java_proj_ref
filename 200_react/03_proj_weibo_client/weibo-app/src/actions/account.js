d8fc9543caac3bdcd461fa8d32b39f46027e7e56
fangkun119 <fangkun119@hotmail.com>
2020-11-20 15:44:49 +0800

    [200 React] proj03 weibo client:oauth login step 3, get and apply access_token, bug fix

--- 200_react/03_proj_weibo_client/weibo-app/src/actions/account.js
+++ 200_react/03_proj_weibo_client/weibo-app/src/actions/account.js
@@ -1,11 +1,12 @@
 import * as api from '../api/account';
 import {message} from 'antd';
+import {ACCESS_TOKEN_KEY} from '../constants'
 
 export function getAccess(params = {}) {
     return async () => {
         try {
             const { access_token } = await api.getAccess(params);
-            localStorage.setItem('weibo_app_access_token', access_token);
+            localStorage.setItem(ACCESS_TOKEN_KEY, access_token);
         } catch (e) {
             message.error('登录失败');
         }
