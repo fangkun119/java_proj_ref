80fa291ed51b12bf21d312a8af852bfbcd8ad2f0
fangkun119 <fangkun119@hotmail.com>
2020-11-21 14:16:22 +0800

    [200 React] proj03 weibo client: fix warnings

--- 200_react/03_proj_weibo_client/weibo-app/src/pages/Home/components/commentsList/index.js
+++ 200_react/03_proj_weibo_client/weibo-app/src/pages/Home/components/commentsList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { List, Avatar, Card, Button } from 'antd';
 import { useDispatch, useMappedState } from 'redux-react-hook';
 import moment from 'moment';
@@ -12,16 +12,27 @@ const CommentsList = ({ id }) => {
     // action获取评论列表后，会由reducer处理并存储在store/state中，这里从store/state提取评论列表
     const { comments = [], page = 0, total } = useMappedState((state) => (state.comments)); 
     
+    // dispatch
     const dispatch = useDispatch();        
-    const handleInfiniteOnLoad = () => {
-        // 滚动条滚到到底部时，触发获取下一页评论列表的action
+    
+    // 滚动条滚到到底部时，触发获取下一页评论列表的action
+    // useCallback(() => {...}, [dispatch, id, page])
+    //    把内联回调函数及依赖项数组作为参数传入 useCallback，它将返回该回调函数的 memoized 版本，该回调函数仅在某个依赖项改变时才会更新
+    //    对这个例子：当disaptch,id,page之一发生变化时，该回调函数也会发生变化
+    // 参考（HookAPI索引）: https://zh-hans.reactjs.org/docs/hooks-reference.html#usecallback
+    const handleInfiniteOnLoad = useCallback(() => { // add 'useCallback' for fix warning "The 'handleInfiniteOnLoad' function makes the dependencies of useEffect Hook (at line 23) change on every render. To fix this, wrap the definition of 'handleInfiniteOnLoad' in its own useCallback() Hook"
         dispatch(getComments({ id, page : page + 1, count: COMMENT_PAGESIZE }));
-    }
-    useEffect(() => {
-        // 组件加载时、以及dispatch发生变化时，触发获取评论列表的action
-        handleInfiniteOnLoad();
-    }, []);
+    }, [dispatch, id, page]); // add dependencies for error "React Hook useCallback does nothing when called with only one argument. Did you forget to pass an array of dependencies?"
 
+    // CommentList组件加载时，触发获取评论列表的action
+    useEffect(() => {
+        handleInfiniteOnLoad();
+    }, []);  
+    // }, [handleInfiniteOnLoad]); 
+    // 不希望 handleInfiniteOnLoad 发生变化时，加载下一页评论，将其从useEffect依赖中移除
+    // 否则会造成死循环（调用handleInfiniteOnLoad会更新page，而更新page会导致handleInfiniteOnLoad发生变化）
+    
+    // 仅当还有未显示的评论时，才会出现“加载更多”按钮
     const loadMore = page * COMMENT_PAGESIZE < total && (
         <div className={styles.loadMore}>
             <Button onClick={handleInfiniteOnLoad}>加载更多</Button>
